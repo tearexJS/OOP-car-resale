d446b1e4b6a1fa722d42a8220c12c88f377b641f
tearexJS <olinovak96@gmail.com>
2022-04-07 21:52:23 +0200

    Created entitities based on the database model

---
+++ DAL/Entities/Credentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Credentials
+    {
+        public int ContactId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ DAL/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
