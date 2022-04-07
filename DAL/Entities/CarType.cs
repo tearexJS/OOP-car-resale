c6933e851acea64da512b9b333c73866cc9aef99
tearexJS <olinovak96@gmail.com>
2022-04-07 21:52:23 +0200

    Created entitities based on the database model

---
+++ DAL/Entities/CarType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class CarType
+    {
+        public int CarTypeId { get; set; }
+
+        public string Type { get; set; }
+
+        public List<CarModel> Models { get; set; }
+    }
+}
