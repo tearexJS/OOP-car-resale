85a2c29e81550aaa6177d41a62a64a8820137764
xmanas07 <tomas.manasek15@seznam.cz>
2022-04-09 22:50:37 +0200

    Added test GUI files
    Tried to learn WPF, this is the result.
    Only a test GUI.

---
+++ GUI/MWM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using GUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.MWM.ViewModel
+{
+
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayComand HomeViewCommand { get; set; }
+        public RelayComand MyAdvertViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public MyAdvertViewModel MyAdvertVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            MyAdvertVM = new MyAdvertViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayComand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            MyAdvertViewCommand = new RelayComand(o =>
+            {
+                CurrentView = MyAdvertVM;
+            });
+        }
+    
+    }
+}
