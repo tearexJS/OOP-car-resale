4211e21decc1c5e04e75ca3e184a316324d6a706
xmanas07 <tomas.manasek15@seznam.cz>
2022-04-09 12:59:16 +0200

    Squashed commit of the following:
    commit c6933e851acea64da512b9b333c73866cc9aef99
    Author: tearexJS <olinovak96@gmail.com>
    Date:   Thu Apr 7 21:52:23 2022 +0200
    
        Created entitities based on the database model

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public List<Advertisement> Advertisements { get; set; }
+
+    }
+}
