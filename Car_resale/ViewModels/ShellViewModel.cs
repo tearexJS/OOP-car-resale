b34d9b0e9786f425946f9f8cf49530e547ed3392
tearexJS <olinovak96@gmail.com>
2022-04-29 19:34:05 +0200

    Added GUI part

---
+++ Car_resale/ViewModels/ShellViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        //public BindableCollection<> Adverts { set; get; }
+        HomeViewModel HomeViewModel = new HomeViewModel();
+        BrowseViewModel BrowseViewModel = new BrowseViewModel();
+        MyAccountViewModel MyAccountViewModel = new MyAccountViewModel();
+        AdvertViewModel AdvertViewModel;
+        NewAdvertViewModel NewAdvertViewModel = new NewAdvertViewModel();
+
+
+        public ShellViewModel()
+        {
+
+            ActivateItemAsync(HomeViewModel);
+        }
+        public void LoadPage1()
+        {
+            ActivateItemAsync(HomeViewModel);
+        }
+
+        public void LoadPage2()
+        {
+            ActivateItemAsync(BrowseViewModel);
+        }
+
+        public void LoadPage3()
+        {
+            ActivateItemAsync(NewAdvertViewModel);
+        }
+
+        public void LoadPageMyAccount()
+        {
+            ActivateItemAsync(MyAccountViewModel);
+        }
+        public void RowView(AdvertsModel advert)
+        {
+            AdvertViewModel = new AdvertViewModel(advert);
+            ActivateItemAsync(AdvertViewModel);
+        }
+
+
+
+
+    }
+}
